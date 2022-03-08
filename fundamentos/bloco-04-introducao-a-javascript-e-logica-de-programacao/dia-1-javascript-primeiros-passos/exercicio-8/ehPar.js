8a17f431de45a35310575a0188d4eb6d13fc09fb
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-07 21:45:00 -0300

    modifiquei nome da funcão

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-8/ehPar.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-8/ehPar.js
@@ -1,7 +1,8 @@
 const num1 = 11;
 const num2 = 23;
 const num3 = 35;
-function ehImpar(num1, num2, num3) {
+
+function ehPpar(num1, num2, num3) {
   if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
     return true;
   } else {
