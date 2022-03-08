897e95e8ca21d63913229872690f7d2dbebb6f92
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-08 18:52:38 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1 - imprime todos valores com a funlçao console.log
+//console.log(numbers);
+
+// 2 - soma e imprime todos valores
+function somaValores(numbers) {
+  let sum = 0;
+  for(let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+//console.log(somaValores(numbers));
+
+// 3 - Média Aritimética
