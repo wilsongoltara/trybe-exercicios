782fcefeff966e98665593f05cbb28463e426fdb
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-15 15:15:52 -0300

    Exercício 3.2 - Menor palavra

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-e-algoritimos/majorAndMinor.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-e-algoritimos/majorAndMinor.js
@@ -1,9 +1,22 @@
 const majorWord = ((arrayOfWords) => {
   let major = arrayOfWords[0];
-  for (let i = 0; i < arrayOfWords.length; i++) {
-    if (arrayOfWords[i].length > major.length) {
-      major = arrayOfWords[i];
+  for (let word of arrayOfWords) {
+    if (word.length > major.length) {
+      major = word;
     }
   }
   return major;
 });
+
+const minorWord = ((arrayOfWords) => {
+  let minor = arrayOfWords[0];
+  for (let word of arrayOfWords) {
+    if (word.length < minor.length) {
+      minor = word;
+    }
+  }
+  return minor;
+});
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+console.log(minorWord(array));
