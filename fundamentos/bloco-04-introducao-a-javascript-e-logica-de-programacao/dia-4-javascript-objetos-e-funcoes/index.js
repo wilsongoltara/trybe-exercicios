6b073a0b900a676f57008d58b2c17c8ad321f9a3
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-21 21:35:46 -0300

    Exercício 3 - For/in imprimindo as chaves

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
@@ -4,10 +4,12 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-
-// 1 - Imprime uma mensagem de boas-vindas para a personagem acima
 //console.log(`Bem-vindo(a), ${info.personagem}`);
 
-// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 info.recorrente = 'Sim';
-console.log(info);
+// console.log(info);
+
+for (let key in info) {
+  console.log(key);
+}
+
