4a8737e0528bec138d2e21ee1f7a3913e9b9ab84
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-21 21:58:18 -0300

    Exercício 6 - Imprime livro favorito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
@@ -26,10 +26,25 @@ let info2 = {
   recorrente: 'Sim'
 }
 
-for (let key in info, info2) {
-  if (key !== 'recorrente') {
-    console.log(info[key] + ' e ' + info2[key]);
-  } else {
-    console.log('Ambos recorrentes');
-  }
-}
+// for (let key in info, info2) {
+//   if (key !== 'recorrente') {
+//     console.log(info[key] + ' e ' + info2[key]);
+//   } else {
+//     console.log('Ambos recorrentes');
+//   }
+// }
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorito de', leitor.nome, leitor.sobrenome, 'se chama', leitor.livrosFavoritos[0].titulo);
