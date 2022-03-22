c1d2ea59d96d057261a3bd614c6e9b5e2f5e7e14
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-22 13:09:08 -0300

    Exercício 2 - retorna index do maior inteiro

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2.js
@@ -1,7 +1,19 @@
 function verificaPalindrome(string) {
-  let reversed = string.split("").reverse().join('');
+  let reversed = string.split('').reverse().join('');
   if (string === reversed) {
     return true;
   }
   return false;
+}
+
+function indexHighestValue(integers) {
+  let highestValue = integers[0];
+  let indexValue = 0;
+  for (let index in integers) {
+    if((integers[index]) > highestValue) {
+      indexValue = parseInt(index);
+      highestValue = integers[index];
+    }
+  }
+  return indexValue;
 }
