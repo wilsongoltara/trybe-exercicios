e68375cc0b55e6d6b3861a11a5cb61f7417821df
wilsongoltara <wilson.goltara15@gmail.com>
2022-03-24 15:25:16 -0300

    update calendat for data current

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/script.js
@@ -1,4 +1,4 @@
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysList = [23, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
@@ -18,7 +18,7 @@ createDaysOfTheWeek();
 function createDaysOFCalendar(days) {
   const daysList = document.getElementById('days');
 
-  for (let index = 0; index < days.length; index += 1) {
+  for (let index = 3; index < days.length; index += 1) {
     const day = days[index];
     const dayListItem = document.createElement('li');
     dayListItem.classList = 'day';
@@ -28,7 +28,7 @@ function createDaysOFCalendar(days) {
       dayListItem.classList.add('holiday');
     }
 
-    if (day === 4 || day === 11 || day === 18 || day === 25) {
+    if (day === 2 || day === 9 || day === 16 || day === 23 || day === 30) {
       dayListItem.classList.add('friday');
     }
 
@@ -47,4 +47,4 @@ function holidaysButton(string) {
   containerButtons.appendChild(btnHolidays);
 }
 
-holidaysButton('Feriados');
+holidaysButton('Feriados'); 
