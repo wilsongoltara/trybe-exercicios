a4a8afe8b80ba96369920ca052b9d58d91795b8f
wilsongoltara <wilson.goltara15@gmail.com>
2022-04-19 12:28:19 -0300

    exercise 1 - function people

--- fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
+++ fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
@@ -1,9 +1,14 @@
 // Request 1
-const newEmployees = () => {
+const newEmployees = (people) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: people('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: people('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: people('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+
+const people = fullName => ({
+  fullName: fullName,
+  email: `${fullName.split(' ').join('_')}@trybe.com`
+});
