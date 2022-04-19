7ff60498622005e06e78309e0a8d261a3225100e
wilsongoltara <wilson.goltara15@gmail.com>
2022-04-19 13:05:55 -0300

    Request 3 - grade final

--- fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
+++ fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
@@ -21,3 +21,28 @@ const resultDraw = (bet, hasWon) => {
   const numberRandom = Math.floor((Math.random() * 5) + 1);
   return hasWon(numberRandom, bet);
 };
+
+// Request 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const verifyAnswers = (rightAnswers, studentAnswers) => {
+  const gradeFinal = studentAnswers.reduce((accGrade, answer, index) => {
+    if (answer === rightAnswers[index]) {
+      accGrade += 1;
+    }
+    
+    if (answer !== rightAnswers[index] && answer !== 'N.A') {
+      accGrade -= 0.5;
+    }
+    
+    return accGrade;
+  }, 0);
+  return gradeFinal;
+};
+
+const grade = (rightAnswers, studentAnswers, verifyAnswers) => {
+  return `Resultado: ${verifyAnswers(rightAnswers, studentAnswers)} pontos`;
+};
+
+console.log(grade(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
