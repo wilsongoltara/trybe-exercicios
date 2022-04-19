f9ff7fd52127e6baac2caea7a3ba16cdc33bf254
wilsongoltara <wilson.goltara15@gmail.com>
2022-04-19 12:42:28 -0300

    Request 2 - Draw

--- fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
+++ fundamentos/bloco-08-higher-order-functions/dia-01-introducao-hof/exercises.js
@@ -1,14 +1,23 @@
 // Request 1
-const newEmployees = (people) => {
+const newEmployees = (employeeGenerator) => {
   const employees = {
-    id1: people('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: people('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: people('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-const people = fullName => ({
-  fullName: fullName,
-  email: `${fullName.split(' ').join('_')}@trybe.com`
-});
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return { fullName, email: `${email}@trybe.com` };
+};
+
+
+//Request 2
+const hasWon = (numberRandom, bet) => numberRandom === bet ? 'Parabéns você ganhou!' : 'Tente Novamente!' ;
+
+const resultDraw = (bet, hasWon) => {
+  const numberRandom = Math.floor((Math.random() * 5) + 1);
+  return hasWon(numberRandom, bet);
+};
